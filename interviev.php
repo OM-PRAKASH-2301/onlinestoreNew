00f2082672ff7d58136dc9abc33fff27b7cb8a31
OM-PRAKASH-2301 <omdewangan01@gmail.com>
2023-07-25 12:54:08 +0530

    changes on 25/07/2023

--- interviev.php
+++ interviev.php
@@ -1,4 +1,47 @@
 HTML Introduction -->
 
 HTML is the standard markup language for creating Web pages.
-HTML stands for hypertext markup language
+HTML stands for hypertext markup language\
+----------------------------------------OOPS--------------------------
+
+OOPS - Object oriented Programming Structure
+POPS - Procedurel oriented Programming structure 
+
+- class (class is a collection of object that share common properties and behaviour, using a class we can create many object of class)
+
+- Object (an object is indivitual instance of data structure defined by class)
+
+Properties of Object 
+- Abstraction - (abstract is using to not use the function outside of class, it is protected for outside class)
+- Inheritance 
+        - Single Inheritance
+        - multiple inheritance 
+        - multilevel inheritance 
+        - hybrid inheritance 
+- Data Binding 
+- Polymorphism (through this we can use function in other classes but only behaviour of function will be changed)
+- Encapsulaion
+
+
+
+-- MethodOverloading and method Overrriding
+-- DDL (data defination language) and DML (data manupulation language)
+-- 
+-------------------------------------------------------------------------------------------
+Object-Oriented Programming (OOP) is a programming paradigm that organizes code into objects that interact with each other. Here are some key properties of Object-Oriented Programming:
+
+Encapsulation: Encapsulation refers to the bundling of data and methods within a single unit, called an object. The object encapsulates the data and provides methods to manipulate and access that data. It helps in hiding the internal details of an object and provides abstraction.
+
+Inheritance: Inheritance allows objects to inherit properties and behaviors from a parent class or base class. The derived classes, also known as child classes or subclasses, can extend or specialize the functionality of the base class. Inheritance promotes code reusability and facilitates hierarchical relationships between classes.
+
+Polymorphism: Polymorphism enables objects of different classes to be treated as objects of a common superclass. It allows methods in different classes to have the same name but different implementations. Polymorphism can be sachieved through method overriding and method overloading.
+
+Abstraction: Abstraction focuses on providing a simplified view of the complex systems by identifying the essential features relevant to the user and hiding unnecessary details. In OOP, abstraction is achieved through abstract classes and interfaces.
+
+Modularity: OOP promotes modularity by breaking down a complex system into smaller, self-contained objects. Each object can be developed and maintained independently, making the code more manageable and reusable.
+
+Association: Association represents a relationship between two or more objects. It can be a one-to-one, one-to-many, or many-to-many relationship. Objects can interact with each other through associations, exchanging information or triggering actions.
+
+Encapsulation: Encapsulation is the concept of bundling data and the methods that operate on that data into a single unit called an object. It provides data protection by controlling access to the object's internal state. Encapsulation helps in achieving data abstraction and enhances code maintainability and reusability.
+
+These properties collectively form the foundation of Object-Oriented Programming and enable developers to build modular, reusable, and maintainable code by organizing it into objects and establishing relationships between them.
